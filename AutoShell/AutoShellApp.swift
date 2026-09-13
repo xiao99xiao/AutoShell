d7ba875ff286e007762fc8253e69845f59900b27
Xiao Xiao <xx2004xiamen@gmail.com>
2026-09-13 12:02:49 +0900

    Initial Commit

---
+++ AutoShell/AutoShellApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoShellApp.swift
+//  AutoShell
+//
+//  Created by Xiao Xiao on 2026/09/13.
+//
+
+import SwiftUI
+
+@main
+struct AutoShellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoShell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoShell
+//
+//  Created by Xiao Xiao on 2026/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
